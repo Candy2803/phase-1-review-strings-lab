3cc223a7acc9f42805f88a66b99d6a6b9901dfb5
+ <+>
2024-06-24 11:01:55 +0300

    committed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "String"
+const welcomeMessage = "Welcome to Flatbook, String!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, STRING!"
+const shortGreeting = "Welcome, S!"
