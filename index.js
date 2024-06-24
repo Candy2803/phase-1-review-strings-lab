41522df6690b4aa1f12e7f1619dc50e9aaf94497
+ <+>
2024-06-24 11:05:51 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 let currentUser = "String"
-const welcomeMessage = "Welcome to Flatbook, String!"
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, STRING!"
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
 const shortGreeting = "Welcome, S!"
